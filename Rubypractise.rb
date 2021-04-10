efac684d914e7eaeea2b810289e141993d8820c1
Chatrughan Prasad <chatrughan.prasad@outlook.com>
2021-04-10 10:20:28 +0530

    Create Rubypractise.rb
    Ruby reference  guide

---
+++ Rubypractise.rb
@@ -0,0 +1,121 @@
+print 'hello world'
+puts 'my friend is vijay'
+puts 'my name is ajay'
+
+a = 1
+if a > 2
+  puts 'a is greater then 2'
+elsif a <= 2 and a != 0
+  puts 'a is 1'
+else
+  puts 'x is smaller then 2'
+end
+
+// != not equal to
+// == equal to
+or matlab koi bhi condition meet hui then show then show the output
+And matlab ek bhi condition meet hui then show then show the output
+i = 0
+num = 5
+while i < num do
+  puts i
+  i+=1
+end
+
+i = 0
+num = 5
+begin
+  puts i
+  i+=1
+end while i < num
+
+for i in 0..5
+  if i > 4 then
+    break
+  end
+  puts i
+end
+
+nums = Array.[1,2,3,4,5,6,7,8,.9, "Vijay"]
+puts nums
+
+my_name = "vijay kumar"
+puts my_name.capitalize
+puts my_name.upcase
+puts my_name.downcase
+puts my_name.reverse
+puts my_name.length
+puts my_name[3..6]
+puts my_name[6]
+
+def test(a1="Vijay", a2="Ajay")
+  puts "the programming language is #{a1}"
+  puts "the programming language is #{a2}"
+end
+
+test "c1", "c2"
+test
+
+def test
+  puts "this is a method"
+  yield 4, 5
+  puts "This is a method2"
+  yield 3, 6
+end
+test do
+  puts {|i| "this is a block #{i}"
+  }
+end
+
+BEGIN {
+  puts "this is begin block"
+}
+END {
+  puts "this is a end block"
+}
+puts "this is a main block"
+puts "this is a main block"
+puts "this is a main block"
+puts "this is a main block"
+
+vijay_hash={
+    "1"=>"vijay",
+    "2"=>"ajay",
+    "3"=>"jay",
+    "4"=>"Arun",
+    "5"=>"Ashok",
+    "6"=>"Mukesh"
+}
+
+puts vijay_hash["1"]
+puts vijay_hash["5"]
+
+vijay_hash = Hash.new
+vijay_hash["1"] = "vijay"
+vijay_hash["2"] = "vijay"
+vijay_hash["3"] = "vijay"
+vijay_hash["4"] = "vijay"
+vijay_hash["5"] = "vijay"
+vijay_hash["6"] = "vijay"
+
+puts vijay_hash["2"]
+
+print "enter your name"
+user = gets.chomp
+puts "your name is #{user}"
+
+array= ["vijay", "ajay", "raman", "negi", "rise"]
+for item in array
+  puts item
+end
+
+multi diemension array
+
+
+number = [[1,2,3],[4,5,6],[7,8,9]]
+array = [["]vijay", "ajay"], ["raman", "negi"], ["rise", "sheen"]]
+
+puts array [1][0][0]
+puts array [1]
+
+puts number[1][1]
